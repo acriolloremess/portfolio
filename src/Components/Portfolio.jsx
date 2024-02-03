7d84f5919c03de444fa94245130006799602e993
acriollor <151791602+acriolloremess@users.noreply.github.com>
2024-02-03 10:45:16 +0000

    add the projects tab

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Mentorshipss.png";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,9 +31,9 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Mentorship Sessions",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+      "Collaborated Collaborated with small NGOs to Optimize Workflows.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
